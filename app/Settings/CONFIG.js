189eb5575039c5620d1941141c835d153ac7764d
Refloow <59449926+Refloow@users.noreply.github.com>
2020-01-06 20:31:37 +0100

    Update CONFIG.js

--- app/Settings/CONFIG.js
+++ app/Settings/CONFIG.js
@@ -1,10 +1,39 @@
 module.exports = {
+
+
+    // Bot account information // 
+
 	accountName: '',
 	password: '',
 	shared_secret: '',
-	identitySecret: '',
-    
-    WELCOME: 'Hi, how can im help you ?',
-    CustomPlayingMessage: '',
-	INVITETOGROUPID: ''
-};
+	
+            // Settings //
+               
+        CustomPlayingMessage: "",                  // Message that will be displayed as playing game
+	    INVITETOGROUPID: '103582791463409634',     // id of the group that bot will invite user after accepting friend request
+        add_message: 'Hi, how can i help you %?',  // Leave the '%' to get it to replace it with the user's name
+
+
+        messages: true,                         // [true/false] senting message after accepting friend request
+        group_inviting: true,                   // [true/false] inviting user to the selected group
+        auto_accept: true,                      // [true/false] Enable or disable automatically accepting friend requests.
+
+
+        minimum_level: 2,                       // Minimum level required to be automatically accepted if setting above is true 
+
+
+
+
+
+
+
+
+
+
+
+   // Non-important settings //
+ 
+        clock: 'en',                                // Language code for your 
+        persona: 'Online',                          // Steam persona status
+        game: [440, 730, 735],
+}
