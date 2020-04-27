a367259dbb3d92bd64b7970c7c17ea94d82f2a35
Refloow <59449926+Refloow@users.noreply.github.com>
2020-04-28 01:34:33 +0200

    Updated to version 1.3.0
    Added checker if modules are installed correctly 
    Added comments

--- app/steam.js
+++ app/steam.js
@@ -1,3 +1,33 @@
-const SteamUser = require('steam-user');
-const client = new SteamUser();
-const SteamTOTP = require('steam-totp');
+// Auto Friend Request - Bot built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowcontact@gmail.com 
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+ */
+
+ // Checking if all modules are correctly installed
+
+try {
+    // Checking if module steam-user is correctly installed
+	SteamUser = require('steam-user');
+	// Checking if module steam-totp is correctly installed
+	SteamTOTP = require('steam-totp');
+} catch (ex) {
+	// If modules are not installed showing an clear error message to user.
+	console.log('| [Modules] |: Missing dependencies. Install a version with dependecies or use npm install.');
+	console.log(ex);
+	process.exit(1);
+}
+
+// Client
+const refloow = new SteamUser();
+
+
+// Auto Friend Request - Bot built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowcontact@gmail.com 
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+ */
