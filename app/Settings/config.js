e0d5d2897ff850f0b2ac77024c64c77980487b2b
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-01-03 01:21:34 +0100

    Added note

--- app/Settings/config.js
+++ app/Settings/config.js
@@ -1,15 +1,29 @@
 module.exports = {
 
 // Auto Friend Request - Bot built by Refloow (-MajokingGames)
+	
+	    
+/*
+
+Want active support and new updates with new features all for free?
+
+Leave an star on github repo its free ( we push updates based on the engagement )
+Repo link: https://github.com/OSL-Works/Steam-Auto-Friend 
+
+  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
+
+*/
+	
 
 /* 
   Here is contact info: refloowlibrarycontact@gmail.com
   main dev steam: https://steamcommunity.com/id/MajokingGames/
-  Support Discord: https://discord.gg/nNSDPvKBjt
   
   Donate: https://ko-fi.com/refloow
 
  */
+
+
 
 
     // Bot account information // 
