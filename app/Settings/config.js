cb1a99f9b542d3b805b4044d713e109bf1ee806c
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-10 19:06:49 +0100

    Added notes

--- app/Settings/config.js
+++ app/Settings/config.js
@@ -1,6 +1,9 @@
 module.exports = {
 
-// Auto Friend Request - Bot built by Refloow (-MajokingGames)
+// Copyright notice:
+
+/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Auto-Friend*/
 	
 	    
 /*
