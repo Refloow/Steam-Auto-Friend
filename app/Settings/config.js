ba0707131e6b27e10f1a2af8f5b786b7423d867a
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2024-06-21 19:23:07 +0200

    Update config.js

--- app/Settings/config.js
+++ app/Settings/config.js
@@ -2,7 +2,7 @@ module.exports = {
 
 // Copyright notice:
 
-/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+/* Original work: Copyright (c) 2020-2024 Veljko Vuckovic (Refloow.com) All rights reserved.
   Code origin (Free GitHub publish): https://github.com/Refloow/Steam-Auto-Friend*/
 	
 	    
